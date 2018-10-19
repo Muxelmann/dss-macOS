e56b199b5999545377c2c7eb9de1a155baf0b731
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2018-10-19 14:59:37 +0200

    Added macOS app (with GUI)

---
+++ dss_app/dss_app/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  dss_app
+//
+//  Created by Maximilian Zangs on 19.10.18.
+//  Copyright © 2018 Max Zangs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ dss_app/dss_app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  dss_app
+//
+//  Created by Maximilian Zangs on 19.10.18.
+//  Copyright © 2018 Max Zangs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
